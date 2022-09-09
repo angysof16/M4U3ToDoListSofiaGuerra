d3ce9eeae706aa24eee80d925afaebb44d4cfd93
Sofia <sofiguerraj@gmail.com>
2022-09-09 17:37:43 -0500

    ToDo App FIMLM

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,45 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
 import ListaTareas from "./components/ListaTareas";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState({});
+
+  useEffect(() => {
+    const obtenerTareasLocalStorage = () =>{
+      const tareasLocalStorage = JSON.parse(localStorage.getItem("tareas")) ?? [];
+      setTareas(tareasLocalStorage) 
+    };
+    obtenerTareasLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+  }, [tareas]);
+
+  const eliminarTarea = (id) =>{
+    const ActualizarTarea = tareas.filter((tarea) => tarea.id !== id);
+    setTareas(ActualizarTarea);
+  }
+
   return (
     <div className="container mx-auto mt-20">
       <Header />
       <div className="mt-12 md:flex">
-        <Form />
-        <ListaTareas />
+        <Form
+          tareas = {tareas}
+          setTareas = {setTareas}
+          tarea = {tarea}
+          setTarea = {setTarea}
+        />
+        <ListaTareas
+          tareas = {tareas}
+          setTarea = {setTarea}
+          eliminarTarea = {eliminarTarea}
+        />
       </div>
     </div>
   );
--- src/components/AlertError.jsx
+++ src/components/AlertError.jsx
@@ -1,8 +1,7 @@
-
-const AlertError = () => {
+const AlertError = ({ children }) => {
   return (
     <div className="bg-red-600 font-bold uppercase text-center text-white p-3 mb-5 rounded-md">
-      Alerta
+      {children}
     </div>
   );
 };
